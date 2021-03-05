d87b0d63a57d76e1fefebd5dbaddb58af94a6d81
keisuketakahashi514 <takahashikeisuke.0514@gmail.com>
2021-03-05 09:59:19 +0000

    [Add] indexページ作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,21 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/:id'
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
