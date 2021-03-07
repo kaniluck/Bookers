5e01b98d82fc9d19c16e8f880525eff80c4b15c6
keisuketakahashi514 <takahashikeisuke.0514@gmail.com>
2021-03-07 08:31:05 +0000

    [Add] レイアウト等

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,33 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
+  def update
+    book = Book.find(params[:id])
+    book.update!(book_params)
+    redirect_to book_path(book)
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books/:id'
+    redirect_to book_path(book.id)
   end
 
   private
   # ストロングパラメータ
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 
 end
