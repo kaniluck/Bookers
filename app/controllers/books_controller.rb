37bfba2d5f88f46fd355c1baf5bc3f02543691a1
keisuketakahashi514 <takahashikeisuke.0514@gmail.com>
2021-03-07 10:10:59 +0000

    [Last] 残り空白チェックのみ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,18 +15,21 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update!(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book)
   end
 
   def create
     book = Book.new(book_params)
     book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
